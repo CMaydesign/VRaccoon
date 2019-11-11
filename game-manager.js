724c4c7da03ce2ef2fe312d423b80da9bbe2c473
MaozK <47024528+MaozK@users.noreply.github.com>
2019-11-10 19:42:26 -0800

    Environment with fire+light

--- game-manager.js
+++ game-manager.js
@@ -1,53 +1,39 @@
+  
 if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available.');
 }
-document.addEventListener('DOMContentLoaded', function() {
-    const scene = document.querySelector('a-scene');
-    const splash = document.querySelector('#splash');
-    const loading = document.querySelector('#splash .loading');
-    const startButton = document.querySelector('#splash .start-button');
 
-    const emitEvent = (eventName, listeners) => {
-        listeners.forEach((listener) => {
-            const el = document.querySelector(listener);
-            el.emit(eventName);
-        })
-    };
-
-    const emitMediaEvent = (eventType, listeners) => {
-        listeners.forEach((listener) => {
-            const el = document.querySelector(listener);
-            el.components.sound[eventType]();
-        })
-    };
-
-
-    scene.addEventListener('loaded', function (e) {
-        setTimeout(() => {
-            loading.style.display = 'none';
-            splash.style.backgroundColor = 'rgba(0, 0, 0, 0.85)';
-            startButton.style.opacity = 1;
-        }, 50);
-    });
-
-    startButton.addEventListener('click', function (e) {
-        splash.style.display = 'none';
-        emitEvent('scene-started', ['#groundTexture', '#text', '#bushes', '#raccoon', '#trees']);
-    });
-});
-// var hover = new Boolean (false);
-//var temppos;
+var tempBushPos;
+var tempFlamePos;
 
 AFRAME.registerComponent('game-manager', {
     schema: {
         numberTrees: { type: 'int' },
         numberBush: { type: 'int'},
-        //numberText: { type: 'int'}
+        numberText: { type: 'int'},        
+        numberFlames: { type: 'int'},
     },
-    init: function () {
+
+    init: function () 
+	{   
+		
+		var sceneEl = document.querySelector('a-scene');
         var numTrees = this.data['numberTrees'];
-        var sceneEl = document.querySelector('a-scene');
         var newTrees = [];
+        var numBush = this.data['numberBush'];
+        var newBush = [];
+        var numFlame = this.data['numberFlames'];
+		var numLight = numFlame;
+        var newFlame = [];
+        var newLight = [];
+        var numText = this.data['numberText'];
+        var newText = [];
+
+
+		
+        
+
+
         for (var i = 0; i < numTrees; i++) {
             newTrees.push(GameManagerUtils.createTree());
         }
@@ -56,25 +42,42 @@ AFRAME.registerComponent('game-manager', {
                 sceneEl.appendChild(tree);
             });
         });
-        var numBush = this.data['numberBush'];
-        var newBush = [];
-        //var numText = this.data['numberText'];
-        //var newText = [];
         for (var i = 0; i < numBush; i++) {
             newBush.push(GameManagerUtils.createBush());
-            //newText.push(GameManagerUtils.createText());
+            newText.push(GameManagerUtils.createText());
         }
         sceneEl.addEventListener('loaded', function () {
             newBush.forEach(function (bush) {
                 sceneEl.appendChild(bush);
             });
         });
-//         sceneEl.addEventListener('loaded', function () {
-//             newText.forEach(function (text) {
-//                 sceneEl.appendChild(text);
-//             });
-//       });
-    }
+        sceneEl.addEventListener('loaded', function () {
+            newText.forEach(function (text) {
+                sceneEl.appendChild(text);
+            });
+        });
+
+        for (var i = 0; i < numFlame; i++) {
+
+        	
+            newFlame.push(GameManagerUtils.createFlame());
+            newLight.push(GameManagerUtils.createLight());
+           
+            
+
+        }
+        sceneEl.addEventListener('loaded', function () {
+            newFlame.forEach(function (flame) {
+                sceneEl.appendChild(flame);
+            });
+        });
+        sceneEl.addEventListener('loaded', function () {
+            newLight.forEach(function (flamelight) {
+                sceneEl.appendChild(flamelight);
+               
+            });
+        });
+    },
 });
 
 var GameManagerUtils = {
@@ -91,13 +94,11 @@ var GameManagerUtils = {
     createTree: function () {
         console.log('createTree');
         var newTree = document.createElement('a-entity');
-        newTree.setAttribute('id', 'tree');
         newTree.setAttribute('obj-model', 'obj:#tree-model');
         newTree.setAttribute('scale', '1.5 1.5 1.5');
         newTree.setAttribute('material', 'src:tree/bubing/BubingaTree_BaseColor.png');
         // newTree.setAttribute('cursor-listener', '');
         var position = GameManagerUtils.chooseRandomPosition();
-        var positionStr = position.x.toString() + ' ' + position.y.toString() + ' ' + position.z.toString();
         newTree.setAttribute('position', position);
         return newTree;
     },
@@ -105,41 +106,66 @@ var GameManagerUtils = {
         console.log('createBush');
         var newBush = document.createElement('a-entity');
         newBush.setAttribute('template', 'src:bush.template');
-        newBush.setAttribute('cursor-listener');
+        // newTree.setAttribute('cursor-listener', '');
         var position = GameManagerUtils.chooseRandomPosition();
-        var positionStr = position.x.toString() + ' ' + position.y.toString() + ' ' + position.z.toString();
         newBush.setAttribute('position', position);
-        //temppos = position;
-        return newBush;    },
+        tempBushPos = position;
+        return newBush;    
+	},
     
-    // //text
-    // createText: function () {
-    //     if (hover = true){
-    //     console.log('createText');
-    //     var newText = document.createElement('a-entity');
-    //     newText.setAttribute('rotation', '0 200 0');
-    //     //newText.setAttribute('id', 'opacity');
-    //     //newText.setAttribute('animation', 'property: components.text.material.uniforms.opacity.value; to: 0; dir: alternate; loop: true');
-    //     newText.setAttribute('text', 'value: Grab Berries; width:10; align: center;');
-    //     //newText.setAttribute('material', 'color:white;');
-    //     //newText.setAttribute('value', 'Grab berries');
-    //     var position = GameManagerUtils.chooseRandomPosition();
-    //     var positionStr = position.x.toString() + ' ' + position.y.toString() + ' ' + position.z.toString();
-    //     newText.setAttribute('position', temppos);
-    //     newText.setAttribute('position', {y: 2});
-    //     return newText;
-    // }
-    // }
+    //text
+    createText: function () {
+        console.log('createText');
+        var newText = document.createElement('a-entity');
+        newText.setAttribute('rotation', '0 200 0');
+        //newText.setAttribute('id', 'opacity');
+        //newText.setAttribute('animation', 'property: components.text.material.uniforms.opacity.value; to: 0; dir: alternate; loop: true');
+        newText.setAttribute('text', 'value: Grab Berries; width:10; align: center;');
+        //newText.setAttribute('material', 'color:white;');
+        //newText.setAttribute('value', 'Grab berries');
+        newText.setAttribute('position', tempBushPos);
+        newText.setAttribute('position', {y: 2});
+        return newText;
+    },
+
+
+
+    
+     createFlame: function () {
+
+     	
+        console.log('createFlame');
+        var newFlame = document.createElement('a-entity');
+        newFlame.setAttribute('fire', 'particles: 800');
+        var position = GameManagerUtils.chooseRandomPosition();
+        newFlame.setAttribute('position', position);
+        tempFlamePos = position;
+	    return newFlame;
+	    
+	    
+	},
+    
+     createLight: function () {
+        console.log('createLight');
+        var newLight = document.createElement('a-light');
+        newLight.setAttribute('light', 'type: point; color: #FFA200; intensity: 3; distance: 20');
+        newLight.setAttribute('position', tempFlamePos);
+        newLight.setAttribute('position', {y: 0.5});
+        return newLight;
+    }
+
+    
     
 };
+
 // Component to change to a sequential color on click.
 AFRAME.registerComponent('cursor-listener', {
   init: function () {
     var lastIndex = -1;
-    this.el.addEventListener('hover', function (evt) {
+    var COLORS = ['red', 'green', 'blue'];
+    this.el.addEventListener('click', function (evt) {
       lastIndex = (lastIndex + 1) % COLORS.length;
       this.setAttribute('visibility', 'true');
-      hover = true;
       console.log('I was clicked at: ', evt.detail.intersection.point);
     });
   }
@@ -186,3 +212,138 @@ AFRAME.registerComponent('random-rotation', {
     });
   }
 });
+
+//from fire.js
+const VERTEX = `
+    precision highp float;
+    #define PI 3.1415926535897932384626433832795
+    uniform mat4 modelViewMatrix;
+    uniform mat4 projectionMatrix;
+    uniform float time;
+    uniform float size;
+    
+    attribute vec3 position;
+    attribute vec3 direction;
+    attribute float offset;
+    varying vec3 vUv;
+    void main() {
+        float sawTime = mod(time * offset, PI);
+        float sineTime = (sawTime * abs(sin(time * offset)));
+        vec3 timeVec = vec3(sineTime, sawTime, sineTime);
+        vUv = ((normalize(position) * 0.2) + (timeVec * direction)) * size;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( vUv, 1.0 );
+    }
+    `;
+
+const FRAGMENT = `
+    precision highp float;
+    uniform float time;
+    uniform float yMax;
+    varying vec3 vUv;
+    float random(vec2 ab) {
+        float f = (cos(dot(ab ,vec2(21.9898,78.233))) * 43758.5453);
+        return fract(f);
+    }
+    void main() {
+        float alpha = (yMax - vUv.y) * 0.8;
+        float red = 1.0;
+        float green = 0.3 + (0.7 * mix(((yMax - vUv.y) * 0.5) + 0.5, 0.5 - abs(max(vUv.x, vUv.y)), 0.5));
+        float blueMin = abs(max(max(vUv.x, vUv.z), (vUv.y / yMax)));
+        float blue = (1.0 / (blueMin + 0.5)) - 1.0;
+        gl_FragColor = vec4(red, green, blue, alpha);
+    }
+    `;
+
+const createSparks = (count) => {
+        const positions = [];
+        const directions = [];
+        const offsets = [];
+        const verticesCount = count * 3;
+
+        for (let i = 0; i < count; i += 1) {
+            const direction = [
+                Math.random() - 0.5,
+                (Math.random() + 0.3),
+                Math.random() - 0.5];
+            const offset = Math.random() * Math.PI;
+
+            const xFactor = 1;
+            const zFactor = 1;
+
+            for (let j = 0; j < 3; j += 1) {
+                const x = Math.random() - 0.5;
+                const y = Math.random() - 0.2;
+                const z = Math.random() - 0.5;
+
+                positions.push(x, y, z);
+                directions.push(...direction);
+                offsets.push(offset);
+            }
+        }
+
+        const geometry = new THREE.BufferGeometry();
+
+        geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+        geometry.addAttribute('direction', new THREE.Float32BufferAttribute(directions, 3));
+        geometry.addAttribute('offset', new THREE.Float32BufferAttribute(offsets, 1));
+
+        return geometry;
+    };
+
+AFRAME.registerComponent('fire', {
+        schema: {
+            particles: {
+                type: 'number',
+                default: 1000
+            },
+            size: {
+                type: 'number',
+                default: 0.5,
+            },
+        },
+        init() {
+            this.data.size = Math.random() * 2.5 + 1.5;
+            const size = this.data.size;
+
+            this.material = new THREE.RawShaderMaterial({
+                uniforms: {
+                    time: { value: 0.0 },
+                    size: { value: size },
+                    yMax: { value: 0.3 + Math.PI * size },
+                },
+                vertexShader: VERTEX,
+                fragmentShader: FRAGMENT,
+                side: THREE.DoubleSide,
+                transparent: true,
+            });
+
+            this.object3D = new THREE.Object3D();
+            this.el.setObject3D('mesh', this.object3D);
+        },
+        update() {
+            const data = this.data;
+            const size = data.size;
+
+            if (this.mesh) {
+                this.object3D.remove(this.mesh);
+            }
+
+            this.material.uniforms.size.value = size;
+
+            const geometry = createSparks(data.particles);
+            this.mesh = new THREE.Mesh(geometry, this.material);
+
+            this.object3D.add(this.mesh);
+        },
+        remove() {
+            const curr = this.el.getObject3D('mesh')
+            if (curr) {
+                this.object3D.remove(curr);
+            }
+        },
+        tick(time) {
+            this.material.uniforms.time.value = time * 0.0005;
+        },
+});
+
+
